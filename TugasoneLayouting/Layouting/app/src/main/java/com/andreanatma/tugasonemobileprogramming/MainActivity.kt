3398671276d611c2dbe7eb49a3bcf21c3f66ee9c
andreanatma <146343705+andreanatma@users.noreply.github.com>
2024-11-17 08:28:48 +0700

    Praktikum 1

---
+++ TugasoneLayouting/Layouting/app/src/main/java/com/andreanatma/tugasonemobileprogramming/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.andreanatma.tugas1layouting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
